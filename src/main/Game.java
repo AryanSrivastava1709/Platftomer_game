be12dfc93565fa95d3b2cf653f42e9ec5d63c877
Aryan Srivastava <aryansrivas123@gmail.com>
2023-04-20 15:10:14 +0530

    Added Levels,Minor Bug fixes

--- src/main/Game.java
+++ src/main/Game.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import gamestates.Gamestate;
 import gamestates.Menu;
 import gamestates.Playing;
+import utilz.LoadSave;
 
 public class Game implements Runnable {
 
@@ -33,7 +34,6 @@ public class Game implements Runnable {
 		gamePanel.requestFocus();
 
 		startGameLoop();
-
 	}
 
 	private void initClasses() {
